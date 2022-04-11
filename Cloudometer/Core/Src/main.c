4c4ae4e107139fee9d5425eacaa4e0b6a48fce42
PalmerEldritch <jeppeshot@hotmail.com>
2022-04-11 14:59:54 +0200

    Syscalls

--- Cloudometer/Core/Src/main.c
+++ Cloudometer/Core/Src/main.c
@@ -18,7 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
+// HEJHEJ!
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
